d3e53c13ab5897937221159559aad7478b68655b
Stéphane Deluce <boluge@gmail.com>
2015-02-01 12:20:33 +0100

    init

---
+++ pages/home.php
@@ -0,0 +1,2 @@
+<h1>Je suis la home page</h1>
+<p><a href="index.php?p=single">Aller sur la Single</a></p>
---
+++ pages/template/default.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"/>
+
+    <title>Starter Template for Bootstrap</title>
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Home page</a>
+        </div>
+
+    </div>
+</nav>
+
+<div class="container">
+
+    <div class="starter-template" style="padding-top: 90px;">
+        <?= $content; ?>
+    </div>
+
+</div><!-- /.container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+
+</body>
+</html>
